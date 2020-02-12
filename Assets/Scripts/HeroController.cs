6362791e051077773a2c2f8fdeee7dd1af2d597c
videlanicolas <videlanicolas@gmail.com>
2020-02-12 22:02:37 +1300

    Hero movement left and right with flip animation.

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -7,11 +7,11 @@ public class HeroController : MonoBehaviour
     [Range(0.01f, 0.1f)]
     public float speed = 0.1f;
     [Range(1f, 20f)]
-    public float jumpForce =1f;
+    public float jumpForce = 1f;
 
     float   flipTh = 0.01f,
             horizontalMovement;
-    bool    jump;
+    bool    jump, right;
     Animator animator;
     Rigidbody2D rigidBody;
     // Start is called before the first frame update
@@ -19,6 +19,7 @@ public class HeroController : MonoBehaviour
     {
         animator = gameObject.GetComponent<Animator>();
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
+        right = true;
     }
 
     // Update is called once per frame
@@ -31,7 +32,23 @@ public class HeroController : MonoBehaviour
     private void FixedUpdate()
     {
         rigidBody.MovePosition(new Vector2(rigidBody.position.x + horizontalMovement * speed, rigidBody.position.y));
-        gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        if (horizontalMovement > flipTh)
+        {
+            if (!right)
+            {
+                right = true;
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+
+        }
+        else if (horizontalMovement < -flipTh) {
+            if (right)
+            {
+                right = false;
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+        }
+        
     }
 
 
