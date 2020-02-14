d6f84a8c07923e5cd6ff411d084e6a340e208f54
videlanicolas <videlanicolas@gmail.com>
2020-02-14 12:46:26 +1100

    Revert "Fixed Jump physics."
    This reverts commit 6ded60754e4abdff830c3b4ade61bb57c44051c0.

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -8,38 +8,30 @@ public class HeroController : MonoBehaviour
     public float speed;
     [Range(1f, 20f)]
     public float jumpForce;
-    [SerializeField]
-    public LayerMask groundLayer;
 
     float   flipTh = 0.01f,
             horizontalMovement;
     bool    jump, onGround, prevGround;
     Animator animator;
     Rigidbody2D rigidBody;
-    BoxCollider2D boxCollider;
     // Start is called before the first frame update
     void Awake()
     {
         animator = gameObject.GetComponent<Animator>();
         rigidBody = gameObject.GetComponent<Rigidbody2D>();
-        boxCollider = gameObject.GetComponent<BoxCollider2D>();
         onGround = true;
         prevGround = true;
-        jump = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
-        // Update runs with every frame, we get the "Jump" input here because it's the fastest way posible.
-        // FixedUpdate will check for "Jump", that's why we need to save the state of "Jump" until it's consumed by FixedUpdate.
-        // This leads to no input loss on one-time key presses.
-        if (!jump && onGround) jump = Input.GetButtonDown("Jump");
-        Debug.Log("Jump: " + jump);
+        jump = Input.GetButtonDown("Jump");
+
         onGround = CheckGround();
         if (prevGround != onGround) {
-            Debug.Log("onGround changed: " + onGround);
+            Debug.Log(onGround);
             prevGround = onGround;
         }
     }
@@ -48,10 +40,6 @@ public class HeroController : MonoBehaviour
     {
         if (jump && onGround) {
             onGround = false;
-            // Since the physics engine did it's thing, we consume the jump button and set it again to false.
-            // This allows Update to assign the bool value again.
-            // jump = false;
-            jump = false;
             rigidBody.AddForce(jumpForce * transform.up, ForceMode2D.Impulse);
         }
 
@@ -63,11 +51,13 @@ public class HeroController : MonoBehaviour
     }
 
     bool CheckGround() {
-        float extraY = 0.5f;
-        RaycastHit2D hit = Physics2D.Raycast((Vector2)boxCollider.bounds.center, Vector2.down, boxCollider.bounds.extents.y + extraY, groundLayer);
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position, Vector2.down, 0.75f, 10);
+        Debug.DrawLine((Vector2)transform.position, (Vector2)transform.position + (Vector2.down * 0.75f), Color.red, 1, false);
         if (hit.collider != null) {
+            Debug.Log(hit.point);
             return true;
         }
+
         return false;
     }
 }
