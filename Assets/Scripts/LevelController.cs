e9a6d2f625080d9c629c5ef67e38a0d96d5a5883
videlanicolas <videlanicolas@gmail.com>
2020-02-17 01:27:05 +1300

    Add a canvas with a panel to fade out when there's a game over.

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class LevelController : MonoBehaviour
@@ -7,10 +9,11 @@ public class LevelController : MonoBehaviour
     public AudioClip winMusic, loseMusic;
 
     AudioSource audioSource;
-
+    GameObject canvas;
     private void Awake()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
+        canvas = GameObject.FindGameObjectWithTag("Canvas");
     }
     // Start is called before the first frame update
     void Start()
@@ -34,5 +37,20 @@ public class LevelController : MonoBehaviour
     {
         audioSource.Stop();
         audioSource.PlayOneShot(loseMusic);
+        StartCoroutine(ReloadScene());
     }
+
+    IEnumerator ReloadScene()
+    {
+        yield return new WaitForSeconds(3);
+        // Fade to black.
+        for (float i = 0; i < 255; i++)
+        {
+            yield return new WaitForSeconds(1f);
+            canvas.GetComponentInChildren<Image>().color = new Color(0, 0, 0, i);
+        }
+        SceneManager.LoadScene("Level1");
+    }
+
+
 }
